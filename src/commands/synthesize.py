18a857b0c3beaebe533e234ab5a45b4b5c1b6951
haruyq <156767674+haruyq@users.noreply.github.com>
2026-07-31 20:57:28 +0900

    オプションの追加とUX改善

--- src/commands/synthesize.py
+++ src/commands/synthesize.py
@@ -14,19 +14,19 @@ config = get_config()
 
 Log = Logger(__name__)
 
-@app_commands.context_menu(name="TTSを作成")
-@app_commands.checks.cooldown(1, 5.0, key=lambda i: i.guild.id)
-async def synthesize(interaction: discord.Interaction, message: discord.Message):
+async def make_tts(interaction: discord.Interaction, message: discord.Message, self_speaker: bool = False):
     await interaction.response.defer()
 
     content = message.clean_content
+    user = message.author if self_speaker else interaction.user
 
     if message.attachments:
         attachment_content = describe_attachments(message.attachments)
         content = f"{attachment_content}、{content}" if content else attachment_content
 
-    speech_text = await apply_filters(message.author.id, content)
-    plugin, speaker, style = await get_speaker(message.author.id)
+    speech_text = await apply_filters(user.id, content)
+    
+    plugin, speaker, style = await get_speaker(user.id)
     
     data = {
         "plugin": plugin,
@@ -48,5 +48,16 @@ async def synthesize(interaction: discord.Interaction, message: discord.Message)
                 Log.error(f"Failed to synthesize TTS: {resp.status} - {await resp.text()}")
                 await interaction.followup.send(f"生成に失敗しました。\nHTTP: {resp.status}", ephemeral=True)
 
+@app_commands.context_menu(name="あなたの声で生成")
+@app_commands.checks.cooldown(1, 5.0, key=lambda i: i.guild.id)
+async def synthesize(interaction: discord.Interaction, message: discord.Message):
+    await make_tts(interaction, message, self_speaker=True)
+    
+@app_commands.context_menu(name="送信者の声で生成")
+@app_commands.checks.cooldown(1, 5.0, key=lambda i: i.guild.id)
+async def synthesize_other(interaction: discord.Interaction, message: discord.Message):
+    await make_tts(interaction, message, self_speaker=False)
+
 async def setup(bot: commands.Bot):
     bot.tree.add_command(synthesize)
+    bot.tree.add_command(synthesize_other)
